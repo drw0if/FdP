adbcdd385e8723a23b87e298ea93e628aec49a68
Aleandro Prudenzano <drw0if@drw0if-laptop.localdomain>
2019-10-17 22:16:02 +0200

    commenti 3.1, 3.2

--- 3/3.1.cpp
+++ 3/3.1.cpp
@@ -1,22 +1,29 @@
+/*
+ * Leggere b ed e e calcolare la potenza b^e, gestendo i casi in cui b ed e possano valere 0
+ */
+
 #include <iostream>
 using namespace std;
 
 int main(int argn, char** argv){
 	
 	int b, e;
-	
+	//Continuo a chiedere di inserire b finché non viene inserito un numero non negativo
 	do{
 		cout << "Inserisci b (non negativo): ";
 		cin >> b;
 	}while(b < 0);
 
+	//Continuo a chiedere di inserire e finché non viene inserito un numero non negativo
 	do{	
 		cout << "Inserisci e (non negativo): ";
 		cin >> e;
 	}while(e < 0);
 
+	//Controllo i casi in cui l'esponente sia 0
 	if(e == 0) cout << ((b == 0) ? "Risultato indefinito\n" : "1\n");
 	else{
+		//Calcolo b^e attraverso moltiplicazioni successive
 		int ris = b, i = 1;
 		while(i < e){
 			ris *= b;
--- 3/3.2.cpp
+++ 3/3.2.cpp
@@ -1,29 +1,37 @@
+/*
+ * Leggere da tastiera m ed n e calcolare il massimo comun divisore tramite l'algoritmo di Euclide
+ */
+
 #include <iostream>
 using namespace std;
 
 int main(int argn, char** argv){
 
 	int m, n;
-	
+	//Continuo a chiedere l'inserimento di m finché non viene inserito un numero positivo	
 	do{
 		cout << "Inserisci m (positivo): ";
 		cin >> m;
 	}while(m < 0);
-	
+
+	//Continuo a chiedere l'inserimento di n finché non viene inserito un numero positivo
 	do{
 		cout << "Inserisci n (positivo): ";
 		cin >> n;
 	}while(n < 0);
 	
 	cout << "Il Massimo Comun Divisore tra " << m << " " << n << " e': ";
-	
+
+	//Calcolo l'MCD attraverso sottrazioni successive
 	do{
+		//Se m è minore di n li scambio
 		if(m < n){
 			int tmp = n;
 			n = m;
 			m = tmp;
 		}
 		m -= n;
+		//Continuo a sottrarre finché i numeri sono diversi
 	}while(m != n);
 
 	cout << m;
